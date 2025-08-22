b55d78d554738b68ec78f6b155f4962e6402eecb
Arikalp <sankalpkrsaini@gmail.com>
2025-08-23 01:00:53 +0530

    some changes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-require('dotenv').config({path: './.env'});
+import dotenv from 'dotenv';
+dotenv.config({ path: './.env' });
+
+import app from './app.js';
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
